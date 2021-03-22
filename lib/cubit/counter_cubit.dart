42d9a9c4cf6e9324a0b3a78a9ec38531f94f5255
rbyre <runar.byre@gmail.com>
2021-03-22 17:17:31 +0100

    working counter cubit

--- lib/cubit/counter_cubit.dart
+++ lib/cubit/counter_cubit.dart
@@ -4,5 +4,11 @@ import 'package:meta/meta.dart';
 part 'counter_state.dart';
 
 class CounterCubit extends Cubit<CounterState> {
-  CounterCubit() : super(CounterInitial());
+  CounterCubit() : super(CounterState(counterValue: 0));
+
+  // add 1 to current counterValue
+  void increment() => emit(CounterState(counterValue: state.counterValue + 1));
+
+  // subtract 1 from current counterValue
+  void decrement() => emit(CounterState(counterValue: state.counterValue - 1));
 }
