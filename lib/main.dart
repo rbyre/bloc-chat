324f57787293bcc17d34ca839e3541f866e73083
rbyre <runar.byre@gmail.com>
2021-03-23 20:37:30 +0100

    fix issue with snackbar getting called multiple times when navigating between screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,26 @@
+import 'package:bloc_chat/logic/cubit/counter_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:bloc_chat/presentation/router/app_router.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
   final AppRouter _appRouter = AppRouter();
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Chat',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
+    return BlocProvider<CounterCubit>(
+      create: (context) => CounterCubit(),
+      child: MaterialApp(
+        title: 'Chat',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+        ),
+        onGenerateRoute: _appRouter.onGenerateRoute,
       ),
-      onGenerateRoute: _appRouter.onGenerateRoute,
     );
-  }
-
-  @override
-  void dispose() {
-    _appRouter.dispose();
-    super.dispose();
   }
 }
