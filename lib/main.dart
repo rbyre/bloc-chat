ac55a64903bee1f275449f300f29a8f8251c38d4
rbyre <runar.byre@gmail.com>
2021-03-24 07:07:15 +0100

    change counter according to connectionstatus using Connectivity plus

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,45 @@
 import 'package:bloc_chat/logic/cubit/counter_cubit.dart';
+import 'package:bloc_chat/logic/cubit/internet_cubit.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:bloc_chat/presentation/router/app_router.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MyApp(
+    appRouter: AppRouter(),
+    connectivity: Connectivity(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
-  final AppRouter _appRouter = AppRouter();
+  final AppRouter appRouter;
+  final Connectivity connectivity;
+
+  const MyApp({
+    Key key,
+    @required this.appRouter,
+    @required this.connectivity,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<CounterCubit>(
-      create: (context) => CounterCubit(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<InternetCubit>(
+          create: (context) => InternetCubit(connectivity: connectivity),
+        ),
+        BlocProvider<CounterCubit>(
+          create: (context) =>
+              CounterCubit(internetCubit: context.read<InternetCubit>()),
+        ),
+      ],
       child: MaterialApp(
         title: 'Chat',
         theme: ThemeData(
           primarySwatch: Colors.purple,
         ),
-        onGenerateRoute: _appRouter.onGenerateRoute,
+        onGenerateRoute: appRouter.onGenerateRoute,
       ),
     );
   }
