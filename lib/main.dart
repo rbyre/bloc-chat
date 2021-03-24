b12c7b8c4a7e72d04f0096b37c9a19fc855bea2c
rbyre <runar.byre@gmail.com>
2021-03-24 10:27:46 +0100

    implement context.watch. Counter and internet status.

--- lib/main.dart
+++ lib/main.dart
@@ -30,8 +30,7 @@ class MyApp extends StatelessWidget {
           create: (context) => InternetCubit(connectivity: connectivity),
         ),
         BlocProvider<CounterCubit>(
-          create: (context) =>
-              CounterCubit(internetCubit: context.read<InternetCubit>()),
+          create: (context) => CounterCubit(),
         ),
       ],
       child: MaterialApp(
