4f82eb907cdfef74a9071a070ff6fee8c1aa4ea4
rbyre <runar.byre@gmail.com>
2021-03-24 17:12:40 +0100

    mindre endringer.. :)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:bloc_chat/logic/cubit/counter_cubit.dart';
 import 'package:bloc_chat/logic/cubit/cubit/firebase_cubit.dart';
 import 'package:bloc_chat/logic/cubit/internet_cubit.dart';
+import 'package:bloc_chat/users.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -10,6 +11,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await FirebaseCubit.addRandomUsers(Users.initUsers);
   runApp(MyApp(
     appRouter: AppRouter(),
     connectivity: Connectivity(),
